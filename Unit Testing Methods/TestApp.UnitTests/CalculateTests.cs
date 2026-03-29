f5e2a149bf6b4e5bee169a48ed0c2d43049c1bd3
Enver Fransazov <enverfransazov@gmail.com>
2026-03-29 15:04:36 +0300

    solutions of calculate method

--- Unit Testing Methods/TestApp.UnitTests/CalculateTests.cs
+++ Unit Testing Methods/TestApp.UnitTests/CalculateTests.cs
@@ -4,22 +4,38 @@ namespace TestApp.UnitTests;
 
 public class CalculateTests
 {
-    [Test]
-    public void Test_Addition()
+    private Calculate calculator;
+
+    [SetUp]
+    public void Setup()
     {
-        // Arrange
-        Calculate calculator = new();
-
-        // Act
-        int actual = calculator.Addition(5, 2);
-
-        // Assert
-        Assert.AreEqual(7, actual, "Addition did not work properly.");
+        calculator = new Calculate();
     }
 
-    [Test]
-    public void Test_Subtraction()
+    [TestCase(2, 5, 7)]
+    [TestCase(123, 666, 789)]
+    [TestCase(14 + 14, 2, 30)]
+    public void Test_Addition(int firstNumber, int secondNumber, int expected)
     {
-        // TODO: Write your test here...
+        // Act
+        int actual = calculator.Addition(firstNumber, secondNumber);
+
+        // Assert
+        Assert.AreEqual(expected, actual, "Addition did not work properly.");
+    }
+
+
+    [TestCase(5, 2, 3)]
+    [TestCase(29, 14, 15)]
+    [TestCase(100, 51, 49)]
+    [TestCase(10 - 7, 1, 2)]
+    [TestCase(10 + 7, 1, 16)]
+    public void Test_Subtraction(int firstNumber, int secondNumber, int expected)
+    {
+        // Act
+        int actual = calculator.Subtraction(firstNumber, secondNumber);
+
+        // Assert
+        Assert.AreEqual(expected, actual, "Subtraction did not work properly.");
     }
 }
