31826c511d71b9cd06b8a8d8defb54d3b4001f6a
Enver Fransazov <enverfransazov@gmail.com>
2026-03-29 19:32:07 +0300

    solution of Grades method

--- Unit Testing Methods/TestApp.UnitTests/GradesTests.cs
+++ Unit Testing Methods/TestApp.UnitTests/GradesTests.cs
@@ -4,10 +4,23 @@ namespace TestApp.UnitTests;
 
 public class GradesTests
 {
+    [TestCase(2.00, "Fail")]
+    [TestCase(2.10, "Fail")]
+    [TestCase(2.49, "Fail")]
+    [TestCase(3.00, "Average")]
+    [TestCase(3.10, "Average")]
+    [TestCase(3.49, "Average")]
+    [TestCase(3.50, "Good")]
+    [TestCase(3.51, "Good")]
+    [TestCase(4.49, "Good")]
+    [TestCase(4.50, "Very Good")]
+    [TestCase(4.51, "Very Good")]
+    [TestCase(5.49, "Very Good")]
+    [TestCase(5.50, "Excellent")]
+    [TestCase(5.51, "Excellent")]
+    [TestCase(6.00, "Excellent")]
     public void Test_GradeAsWords_ReturnsCorrectString(double grade, string expected)
     {
-        // Arrange
-
         // Act
         string actual = Grades.GradeAsWords(grade);
 
@@ -15,10 +28,15 @@ public class GradesTests
         Assert.AreEqual(expected, actual);
     }
 
+    [TestCase(1,"Invalid!")]
+    [TestCase(1.66,"Invalid!")]
+    [TestCase(18,"Invalid!")]
+    [TestCase(6.10, "Invalid!")]
+    [TestCase(9090,"Invalid!")]
+    [TestCase(111,"Invalid!")]
+    
     public void Test_GradeAsWords_ReturnsCorrectString_EdgeCases(double grade, string expected)
     {
-        // Arrange
-
         // Act
         string actual = Grades.GradeAsWords(grade);
 
