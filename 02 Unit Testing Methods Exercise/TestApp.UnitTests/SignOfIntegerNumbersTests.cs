d9ae87f7e65173d0d80b845d35410df30d020d9e
Enver Fransazov <enverfransazov@gmail.com>
2026-03-31 21:05:31 +0300

    feat: solution of Check Sign method

--- 02 Unit Testing Methods Exercise/TestApp.UnitTests/SignOfIntegerNumbersTests.cs
+++ 02 Unit Testing Methods Exercise/TestApp.UnitTests/SignOfIntegerNumbersTests.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 
 namespace TestApp.UnitTests;
 
@@ -12,24 +7,57 @@ public class SignOfIntegerNumbersTests
     [Test]
     public void Test_CheckSign_NotAnIntegerValue_ReturnErrorMessage()
     {
+        // Arrange
+        string number = "#";
+        string expectedErr = "The input is not an integer!";
+
+        // Act
+        string result = SignOfIntegerNumbers.CheckSign(number);
+
+        // Assert 
+        Assert.AreEqual(expectedErr, result);
 
     }
 
     [Test]
     public void Test_CheckSign_ZeroValueInput_ReturnMessageForZero()
     {
+        // Arrange
+        string input = "0";
+        string expected = $"The number {input} is zero.";
 
+        // Act
+        string result = SignOfIntegerNumbers.CheckSign(input);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
     }
 
     [Test]
     public void Test_CheckSign_NegativeValueInput_ReturnMessageForNegativeInteger()
     {
+        // Arrange
+        string input = "-45";
+        string expected = $"The number {input} is negative.";
 
+        // Act
+        string result = SignOfIntegerNumbers.CheckSign(input);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
     }
 
     [Test]
     public void Test_CheckSign_PositiveValueInput_ReturnMessageForPositiveInteger()
     {
+        // Arrange
+        string input = "23";
+        string expected = $"The number {input} is positive.";
 
+        // Act
+        string result = SignOfIntegerNumbers.CheckSign(input);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
     }
 }
