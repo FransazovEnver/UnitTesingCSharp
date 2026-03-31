373d45163c9726a6427df6fb73a8aaaf95c16bb2
Enver Fransazov <enverfransazov@gmail.com>
2026-03-31 20:36:22 +0300

    solution of Prime Factor method

--- 02 Unit Testing Methods Exercise/TestApp.UnitTests/PrimeFactorTests.cs
+++ 02 Unit Testing Methods Exercise/TestApp.UnitTests/PrimeFactorTests.cs
@@ -7,12 +7,28 @@ public class PrimeFactorTests
     [Test]
     public void Test_FindLargestPrimeFactor_PrimeNumber()
     {
-        // TODO: finish the test
+        // Arrange
+        int primeNumber = 71;
+
+        // Act
+        long expectedNumber = PrimeFactor.FindLargestPrimeFactor(primeNumber);
+
+        // Assert
+        Assert.AreEqual(expectedNumber, primeNumber);
     }
 
     [Test]
     public void Test_FindLargestPrimeFactor_LargeNumber()
     {
-        // TODO: finish the test
+        // Arrange
+        long largeNumber = 1234567;
+
+        long expected = 9721;
+
+        // Act
+        long result = PrimeFactor.FindLargestPrimeFactor(largeNumber);
+
+        // Assert
+        Assert.AreEqual(result, expected);
     }
 }
